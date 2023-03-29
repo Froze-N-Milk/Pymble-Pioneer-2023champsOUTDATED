33ebad4fa818d2e9962a68e99d481da77694d5bc
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-03-29 20:38:44 +1100

    Initial commit

---
+++ src/routes/scoreCounter.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	let count: number;
+
+	function increment() {
+		count += 1;
+		alert(count);
+	}
+
+	function decrement() {
+		alert(count);
+		if(count > 0) {
+			count -= 1;
+		}
+	}
+	
+</script>
+
+<style>
+	.wrapper {
+		display: inline-flex;
+		border: none;
+		margin: none;
+	}
+
+	button {
+		padding: none;
+		height: 3rem;
+		border: none;
+		outline: 1px solid #151513;
+		background: #20201D;
+		color: snow;
+		margin: none;
+		aspect-ratio: 1/1;
+	}
+
+	.contents {
+		padding-top: 1rem;
+		padding-bottom: 0;
+		padding-left: 1rem;
+		padding-right: 1rem;
+		width: 6rem;
+		border: 0px;
+  		outline: 1px solid #151513;
+		background: #20201D;
+		color: snow;
+		padding: 1rem;
+		text-align: center;
+	}
+</style>
+
+<div class="wrapper">
+	<button on:click={decrement} >-</button>
+	<input inputmode="numeric" readonly class="contents" value={count} min=0/>
+	<button on:click={increment} >+</button>
+</div>
