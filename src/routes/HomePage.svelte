326e14bf742760f9c9af1189bb126a639ea130c0
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-04-05 20:46:16 +1000

    test file uploads

--- src/routes/HomePage.svelte
+++ src/routes/HomePage.svelte
@@ -3,37 +3,74 @@
 		padding: none;
 		height: calc(3rem + 2px);
 		max-width: 14rem;
-		min-width: 5rem;
+		min-width: calc(7rem - 1px);
 		border: none;
 		background: var(--background);
 		color: snow;
 		margin: none;
-		outline: 1px solid #151513;
+	}
+
+	/* input[type = "file"] {
+		border: 0;
+		clip: rect(0, 0, 0, 0);
+		height: 1px;
+		overflow: hidden;
+		padding: 0;
+		position: absolute !important;
+		white-space: nowrap;
+		width: 1px;
+	}
+	.uploader {
+		padding-top: calc(1.25rem);
+		height: calc(1.75rem + 2px);
+		width: calc(7rem - 1px);
+		border: none;
+		background: #20201D;
+		color: snow;
+		margin: none;
+		text-align: center;
+	} */
+
+	.line-wrapper {
+		display: inline-flex;
+		max-width: 14rem;
 	}
 </style>
 
 <script lang="ts">
-    import { downloadToggle, fileType, ScoutingPage } from "./Utils/stores";
+    import { downloadToggle, fileType, MatchDataArray, ScoutingPage } from "./Utils/stores";
     import MatchScouting from "./MatchScouting/MatchScouting.svelte";
     import PitScouting from "./PitScouting/PitScouting.svelte";
-    import { onMount } from "svelte";
 
     let settingsToggle: boolean = false;
 
+	let matchDataFile: any;
+	let pitScoutingFile: any;
 </script>
 
 <form>
 	<label for="" class="left-column">PIT SCOUTING:</label>
-	<button class="hoverSelfAnnounce" style:--background="#20201D" on:click={() => $ScoutingPage = PitScouting}></button>
+	<div class="line-wrapper">
+		<button class="hoverSelfAnnounce" style:--background="#20201D" on:click={() => $ScoutingPage = PitScouting}></button>
+		
+		<div style="width: 2px;"/>
+		<input type="file" name="pit-uploader" id="pit-uploader" >
+		<label for="pit-uploader" class="uploader hoverSelfAnnounce"> ^ </label>
+	</div>
 
 	<label for="" class="left-column">MATCH SCOUTING:</label>
-	<button class="hoverSelfAnnounce" style:--background="#20201D" on:click={() => $ScoutingPage = MatchScouting}></button>
+	<div class="line-wrapper">
+		<button class="hoverSelfAnnounce" style:--background="#20201D" on:click={() => $ScoutingPage = MatchScouting}></button>
+		
+		<div style="width: 2px;"/>
+		<input type="file" name="match-uploader" id="match-uploader">
+		<label for="match-uploader" class="uploader hoverSelfAnnounce"> ^ </label>
+	</div>
 
 	<p class="sectionHeader">SETTINGS</p>
 		
 	<label for="" class="left-column">{settingsToggle ? "HIDE" : "SHOW"} SETTINGS:</label>
 	<button class="hoverSelfAnnounce" style:--background={settingsToggle ? "#5386E4" : "#20201D"} on:click|preventDefault={() => {settingsToggle = !settingsToggle;}}></button>
-
 
 	{#if settingsToggle}
 	<label for="" class="left-column">DOWNLOAD AUTOMATICALLY:</label>
