67af59528ad70953281bfd835892e04ff924cfc3
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-04-05 11:40:58 +1000

    Pit scouting draft 1

--- src/routes/PitScouting/PitScouting.svelte
+++ src/routes/PitScouting/PitScouting.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { PitScoutingArray } from './../Utils/stores.js';
     import { onMount } from "svelte";
+    import Selector from '../Utils/Selector.svelte';
 
 	const regex = new RegExp("^[0-9]*$");
 
@@ -10,7 +11,17 @@
 				event.preventDefault();
 			}
 		});
-		document.getElementById("teamNumber")?.addEventListener("beforeinput", (event) => {
+		document.getElementById("width")?.addEventListener("beforeinput", (event) => {
+			if (event.data != null && !regex.test(event.data)) {
+				event.preventDefault();
+			}
+		});
+		document.getElementById("length")?.addEventListener("beforeinput", (event) => {
+			if (event.data != null && !regex.test(event.data)) {
+				event.preventDefault();
+			}
+		});
+		document.getElementById("weight")?.addEventListener("beforeinput", (event) => {
 			if (event.data != null && !regex.test(event.data)) {
 				event.preventDefault();
 			}
@@ -18,12 +29,11 @@
 	});
 
 	let formError: string;
-	let teamNumberSelector: string | number = ""
-	$: teamNumberSelector = teamNumberSelector === "" ? $PitScoutingArray[0].teamNumber : teamNumberSelector;
-	$: console.log("selected" + teamNumberSelector);
+	let selectedIndex = 0;
+	$: console.log("selected" + selectedIndex);
 	$: console.log("initial" + $PitScoutingArray[0].teamNumber);
 
-	$: SelectedPitScoutingEntry = $PitScoutingArray.filter(array => array.teamNumber === teamNumberSelector)[0] ?? $PitScoutingArray[0];
+	$: SelectedPitScoutingEntry = $PitScoutingArray.filter(array => array.teamNumber === $PitScoutingArray[selectedIndex].teamNumber)[0] ?? $PitScoutingArray[0];
 
 	$:allowAdvance = SelectedPitScoutingEntry.scouterName !== "" && SelectedPitScoutingEntry.teamNumber !== "" && SelectedPitScoutingEntry.teamNumber !== "";
 	
@@ -37,21 +47,60 @@
 	}
 </script>
 
+<style>
+
+	.buttonLookAlike {
+		/* outline: 1px solid #151513; */
+		background: #151513;
+		/* background: #20201D; */
+		color: snow;
+		margin: none;
+		text-align: center;
+		padding-left: 2rem;
+		padding-right: 2rem;
+		overflow: hidden;
+		border: none;
+		height: calc(3rem);
+		width: 10rem;
+	}
+	
+</style>
 
 <h2 class="sectionHeader">PIT SCOUTING</h2>
 
-<label for="" class="left-column">SCOUT NAME:</label>
-<input autocomplete="off" class="hoverSelfAnnounce" inputmode="text" type="text" placeholder="SCOUT NAME" bind:value={SelectedPitScoutingEntry.scouterName} />
+<form>
+	<label for="" class="left-column">SCOUT NAME:</label>
+	<input autocomplete="off" class="hoverSelfAnnounce" inputmode="text" type="text" placeholder="SCOUT NAME" bind:value={SelectedPitScoutingEntry.scouterName} />
 
-<label for="" class="left-column">TEAM #:</label>
-<input autocomplete="off" id="teamNumber" class="hoverSelfAnnounce" type="text" inputmode="numeric" placeholder="TEAM NUMBER" bind:value={SelectedPitScoutingEntry.teamNumber} />
+	<label for="" class="left-column">TEAM #:</label>
+	<input autocomplete="off" id="teamNumber" class="hoverSelfAnnounce" type="text" inputmode="numeric" placeholder="TEAM NUMBER" bind:value={SelectedPitScoutingEntry.teamNumber} />
 
+	<label for="" class="left-column">DRIVE TRAIN TYPE:</label>
+	<Selector options={["TANK", "SWERVE", "MECANUM", "OTHER"]} bind:value={SelectedPitScoutingEntry.driveTrainType} />
+
+	<label for="" class="left-column">DRIVE TRAIN WIDTH:</label>
+	<input autocomplete="off" id="width" class="hoverSelfAnnounce" type="text" inputmode="numeric" placeholder="WIDTH" bind:value={SelectedPitScoutingEntry.driveTrainSizeWidth} />
+
+	<label for="" class="left-column">DRIVE TRAIN LENGTH:</label>
+	<input autocomplete="off" id="length" class="hoverSelfAnnounce" type="text" inputmode="numeric" placeholder="LENGTH" bind:value={SelectedPitScoutingEntry.driveTrainSizeLength} />
+
+	<label for="" class="left-column">ROBOT WEIGHT:</label>
+	<input autocomplete="off" id="weight" class="hoverSelfAnnounce" type="text" inputmode="numeric" placeholder="WEIGHT" bind:value={SelectedPitScoutingEntry.robotWeight} />
+
+	<p class="sectionHeader">AUTO PATHS:</p>
+	<textarea class="sectionHeader hoverSelfAnnounce" rows="8" bind:value={SelectedPitScoutingEntry.autoPaths}></textarea>
+	
+	<p class="sectionHeader">COMMENTS:</p>
+	<textarea class="sectionHeader hoverSelfAnnounce" rows="8" bind:value={SelectedPitScoutingEntry.comments}></textarea>
+
+
+</form>
 <div class="navbar">
 	
-	<select class="buttonLookAlike hoverSelfAnnounce" bind:value={teamNumberSelector}>
-		{#each $PitScoutingArray.map(array => array.teamNumber) as option}
+	<select class="buttonLookAlike hoverSelfAnnounce" bind:value={selectedIndex}>
+		{#each Array.from(Array($PitScoutingArray.length),(x,i)=>i) as option}
 			<option value={option}>
-				{option}
+				{$PitScoutingArray[option].teamNumber}
 			</option>
 		{/each}
 	</select>
